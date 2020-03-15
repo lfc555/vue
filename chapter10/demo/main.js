67dcc8d8ee004469417612965a4b9fe326087119
lfc <caocao4105271@126.com>
2020-03-15 20:23:04 +0800

    1、10 webpack

---
+++ chapter10/demo/main.js
@@ -0,0 +1,13 @@
+//第一种
+// import './style.css'
+// document.getElementById('app').innerHTML='hello webpack'
+//第二种 使用vue
+//导入Vue框架
+import Vue from 'vue'
+//导入 app.vue组件
+import App from './app.vue'
+//创建Vue根实例 
+new Vue({
+    el:'#app',
+    render:h=>h(App)
+});
